7fae323a9dc7cf926a9294806540ef012550bc54
fabiomla-cloud <68978501+fabiomla-cloud@users.noreply.github.com>
2020-08-04 11:58:09 -0300

    Update interface.py

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -330,13 +330,13 @@ class CarInterface(CarInterfaceBase):
     if ret.enableGasInterceptor:
       ret.gasMaxBP = [0., 9., 35]
       ret.gasMaxV = [0.2, 0.5, 0.7]
-      ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
-      ret.longitudinalTuning.kiV = [0.18, 0.12]
+      ret.longitudinalTuning.kpV = [0.15, 0.1, 0.0625] #
+      ret.longitudinalTuning.kiV = [0.045, 0.03]
     else:
       ret.gasMaxBP = [0.]
       ret.gasMaxV = [0.5]
-      ret.longitudinalTuning.kpV = [3.6, 2.4, 1.5]
-      ret.longitudinalTuning.kiV = [0.54, 0.36]
+      ret.longitudinalTuning.kpV = [0.45, 0.3, 0.1875]
+      ret.longitudinalTuning.kiV = [0.135, 0.09]
 
     return ret
 
